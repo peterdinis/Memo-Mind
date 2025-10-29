605e8338ee3b2172ce4738f9d5ee1d7b9bc74c6e
peterdinis <petter1@pobox.sk>
2025-10-29 22:04:23 +0100

    feat: Update auth pages

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+import { ForgotPasswordForm } from "@/components/auth/ForgotPasswordForm";
+import { NextPage } from "next";
+
+const ForgotPasswordPage: NextPage = () => {
+    return <ForgotPasswordForm />
+}
+
+export default ForgotPasswordPage
