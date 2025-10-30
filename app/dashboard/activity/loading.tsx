29b4dcaf5e33c054a220f9342ae9a0c080801a06
peterdinis <petter1@pobox.sk>
2025-10-30 20:39:44 +0100

    feat: Deleting activity page

--- app/dashboard/activity/loading.tsx
+++
@@ -1,8 +0,0 @@
-import { Spinner } from '@/components/ui/spinner';
-import { Suspense } from 'react';
-
-const Loading: React.FC = () => {
-    return <Suspense fallback={<Spinner />} />;
-};
-
-export default Loading;
