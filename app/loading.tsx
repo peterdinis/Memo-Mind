7616ef589f3f7f7ea46323e8babf942ca8ace32d
peterdinis <petter1@pobox.sk>
2025-10-30 18:54:53 +0100

    feat: New loading.tsx files

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from "@/components/ui/spinner";
+import { FC } from "react";
+
+const Loading: FC = () => {
+    return <Spinner variant={"default"} size={"lg"} />
+}
+
+export default Loading
