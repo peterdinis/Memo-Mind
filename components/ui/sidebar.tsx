5c38bcbcbf752428bc4f96a9202c819468c4c6a4
peterdinis <petter1@pobox.sk>
2025-12-21 18:37:11 +0100

    feat: Forrmatting files

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -143,11 +143,13 @@ function SidebarProvider({
             <TooltipProvider delayDuration={0}>
                 <div
                     data-slot='sidebar-wrapper'
-                    style={{
-                        '--sidebar-width': SIDEBAR_WIDTH,
-                        '--sidebar-width-icon': SIDEBAR_WIDTH_ICON,
-                        ...style,
-                    } as CustomStyle}
+                    style={
+                        {
+                            '--sidebar-width': SIDEBAR_WIDTH,
+                            '--sidebar-width-icon': SIDEBAR_WIDTH_ICON,
+                            ...style,
+                        } as CustomStyle
+                    }
                     className={cn(
                         'group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full',
                         className,
@@ -198,9 +200,11 @@ function Sidebar({
                     data-slot='sidebar'
                     data-mobile='true'
                     className='bg-sidebar text-sidebar-foreground w-(--sidebar-width) p-0 [&>button]:hidden'
-                    style={{
-                        '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
-                    } as CustomStyle}
+                    style={
+                        {
+                            '--sidebar-width': SIDEBAR_WIDTH_MOBILE,
+                        } as CustomStyle
+                    }
                     side={side}
                 >
                     <SheetHeader className='sr-only'>
@@ -649,9 +653,11 @@ function SidebarMenuSkeleton({
             <Skeleton
                 className='h-4 max-w-(--skeleton-width) flex-1'
                 data-sidebar='menu-skeleton-text'
-                style={{
-                    '--skeleton-width': width,
-                } as CustomStyle}
+                style={
+                    {
+                        '--skeleton-width': width,
+                    } as CustomStyle
+                }
             />
         </div>
     );
