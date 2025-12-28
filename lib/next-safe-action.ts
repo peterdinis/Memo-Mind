77efa6552815d8cc39bdc1b5f623786d689dce32
peterdinis <petter1@pobox.sk>
2025-12-28 14:47:55 +0100

    feat: Update next-safe-action

--- lib/next-safe-action.ts
+++ lib/next-safe-action.ts
@@ -3,6 +3,7 @@ import { createSafeActionClient } from 'next-safe-action';
 export const action = createSafeActionClient();
 
 export const authenticatedAction = createSafeActionClient({
+    throwValidationErrors: true,
     handleServerError(e) {
         console.error('Action error:', e);
         return e.message || 'Something went wrong';
